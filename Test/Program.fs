7a29b6f88d728efd76878c91d51ac49a10ecb0e3
Mads Nielsen <post@madsnielsen.dk>
2019-10-25 21:17:49 +0200

    tests for monad laws - and it passes!

---
+++ Test/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open FsCheck
+open TFIO.Trans
+
+[<EntryPoint>]
+let main _ =
+    let leftIdentityProperty x y f = run (bind f (return' x)) y = run (f x) y
+    let rightIdentityProperty m x = run (bind return' m) x = run m x
+    let associativityProperty m f g x = run (bind g (bind f m)) x = run (bind (fun y -> bind g (f y)) m) x
+
+    let config = { Config.Quick with EndSize = 1000; MaxTest = 100; Config.QuietOnSuccess = true; }
+
+    Check.One(config, leftIdentityProperty)
+    Check.One(config, rightIdentityProperty)
+    Check.One(config, associativityProperty)
+    0 // return an integer exit code
